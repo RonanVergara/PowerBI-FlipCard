e1dcc476874e119c8c421c680bb0e9132baeb1f6
Ronan Vergara <Vergara.Michael.Ronan@gmail.com>
2026-05-29 13:25:44 +0800

    Add gitignore and remove build artifacts

--- flipCardVisual/.tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var flipCardVisualFC7B9C3E9690417084C0A63577A86637: IVisualPlugin;
-export default flipCardVisualFC7B9C3E9690417084C0A63577A86637;
